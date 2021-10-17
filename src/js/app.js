3e7c01e9b721167de803d856f91015cb586a2dc2
andrey <sharshovandrey@yandex.ru>
2021-10-17 16:57:19 +0300

    refactoring

--- src/js/app.js
+++ src/js/app.js
@@ -1,10 +1,10 @@
-import Core from '../js/Core/Core.js'
+import Core from '../js/Core/Core.js';
 import Game from "../js/game/Game.js";
 import {preloadPack} from "../js/game/preloadPack.js";
-window.addEventListener('load', init)
+window.addEventListener('load', init);
 
 function init() {
-  const core = Core.get()
+  const core = Core.get();
 
   core.loader.addEventListener("complete", handleComplete);
   core.loader.loadManifest(preloadPack, true, "assets/");
